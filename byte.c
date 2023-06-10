4429b42c198866c2dd6a643548f4be98fbaa2fba
AbhijitSarode <arsarode@hotmail.com>
2023-06-10 22:08:59 +0530

    Disabled 'Software control flow'

--- byte.c
+++ byte.c
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include <ctype.h>
 
-
-
 struct termios orig_termios; // Global variable to store the original terminal attributes
 
 //--- Disable raw mode ---//
@@ -21,7 +19,10 @@ void enableRawMode() {
     atexit(disableRawMode); // atexit() is a function that registers disableRawMode() to be called automatically when the program exits
 
     struct termios raw = orig_termios; // Assign a copy of the original attributes to raw
-    raw.c_lflag &= ~(ECHO | ICANON); // Modify the local flags in the struct raw. ECHO causes each key typed to be printed to the terminal. ICANON disables canonical mode
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); // Modify the local flags in the struct raw. ECHO causes each key typed to be printed to the terminal. ICANON disables canonical mode. ISIG disables Ctrl-C and Ctrl-Z signals. Ctrl-C sends a SIGINT signal to the program, which causes it to terminate, and Ctrl-Z sends a SIGTSTP signal to the program, which causes it to suspend execution. IEXTEN disables Ctrl-V and Ctrl-O signals. Ctrl-V causes the next key you press to be literally inserted into the program input. Ctrl-O restarts output that has been stopped using Ctrl-S
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP); // IXON disables Ctrl-S and Ctrl-Q signals. Ctrl-S stops data from being transmitted to the terminal until Ctrl-Q is pressed. ICRNL disables Ctrl-M, which stands for carriage return, or enter key. When disabled, pressing enter will not translate to a 13 (carriage return) followed by a 10 (line feed). BRKINT disables Ctrl-C and Ctrl-Z signals. INPCK enables parity checking. ISTRIP sets the 8th bit of each input byte to 0.
+    raw.c_oflag &= ~(OPOST); // OPOST disables all output processing features. This includes translating '\n' to '\r\n'
+    raw.c_cflag |= (CS8); // CS8 sets the character size (CS) to 8 bits per byte
 
 
     // --- Disabling echo mode ---//
@@ -37,5 +38,14 @@ int main() {
     //--- Reading keypress from user ---//
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') // Read 1 byte from stadard input into c & keep doing that until there are no more bytes to read
+    {
+        if (iscntrl(c)) // iscntrl() tests whether a character is a control character
+        {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        
+    }
     return 0;
 }
